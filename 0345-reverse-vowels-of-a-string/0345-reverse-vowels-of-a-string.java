f70a276141315b74448552e5dbc6e9806ed6de19
Rakshit <98008663+Rakshit-CSE@users.noreply.github.com>
2023-07-01 00:49:28 +0530

    Time: 602 ms (12.17%), Space: 45.1 MB (15.77%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,37 +1,45 @@
 class Solution {
     public String reverseVowels(String s) {
-        char s1[] = s.toCharArray();
-        int l=0;
-        int r=s1.length-1;
-        while(l<=r)
+        char ch[] = s.toCharArray();
+        int i=0;
+        int j=ch.length-1;
+        while(i<=j)
         {
-            if ((s1[l] == 'a' || s1[l] == 'e' || s1[l] == 'i' || s1[l] == 'o' || s1[l] == 'u' || s1[l] == 'A' || s1[l] == 'E' || s1[l] == 'I' || s1[l] == 'O' || s1[l] == 'U') && (s1[r] == 'a' || s1[r] == 'e' || s1[r] == 'i' || s1[r] == 'o' || s1[r] == 'u' || s1[r] == 'A' || s1[r] == 'E' || s1[r] == 'I' || s1[r] == 'O' || s1[r] == 'U'))
+            if(fun(ch[i])==true && fun(ch[j])==true)
             {
-                char temp = s1[l];
-                s1[l] = s1[r];
-                s1[r] = temp;
-                l++;
-                r--;
+                char temp = ch[i];
+                ch[i] = ch[j];
+                ch[j] = temp;
+                i++;
+                j--;
             }
-            else if ((s1[l] != 'a' && s1[l] != 'e' && s1[l] != 'i' && s1[l] != 'o' && s1[l] != 'u' && s1[l] != 'A' && s1[l] != 'E' && s1[l] != 'I' && s1[l]                 != 'O' && s1[l] != 'U') && (s1[r] == 'a' || s1[r] == 'e' || s1[r] == 'i' || s1[r] == 'o' || s1[r] == 'u' || s1[r] == 'A' || s1[r] == 'E' ||             s1[r] == 'I' || s1[r] == 'O' || s1[r] == 'U'))
+            else if(fun(ch[i])!=true)
             {
-                l++;
+                i++;
             }
-            else if ((s1[l] == 'a' || s1[l] == 'e' || s1[l] == 'i' || s1[l] == 'o' || s1[l] == 'u' || s1[l] == 'A' || s1[l] == 'E' || s1[l] == 'I' || s1[l]             == 'O' || s1[l] == 'U') && (s1[r] != 'a' && s1[r] != 'e' && s1[r] != 'i' && s1[r] != 'o' && s1[r] != 'u' && s1[r] != 'A' && s1[r] != 'E' &&                 s1[r] != 'I' && s1[r] != 'O' && s1[r] != 'U'))
+            else if(fun(ch[j])!=true)
             {
-                r--;
-            }
-            else{
-                l++;
-                r--;
-            }
+                j--;
+            } 
         }
-        String ans="";
-        for(int k=0;k<s1.length;k++)
+        String ans = "";
+        for(int k=0;k<ch.length;k++)
         {
-            ans+=s1[k];
+            ans+=ch[k];
         }
         return ans;
+    }
+    public boolean fun(char c)
+    {
+        char arr[] = {'a','e','i','o','u','A','E','I','O','U'};
+        for(int i=0;i<arr.length;i++)
+        {
+            if(c==arr[i])
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
