be8ae9ac30bd7fb27be5704f5291c9119055a16c
Rakshit <98008663+Rakshit-CSE@users.noreply.github.com>
2023-07-05 19:23:50 +0530

    Time: 1 ms (18.21%), Space: 39.4 MB (51.29%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        for(long i=1;;i++)
+        {
+            if(i*i==num)
+            {
+                return true;
+            }
+            if(i*i>num)
+            {
+                return false;
+            }
+        }
+    }
+}
