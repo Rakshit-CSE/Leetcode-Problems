e70e80d72ef49185b9b32d50952adbd5ad5d80e2
Rakshit <98008663+Rakshit-CSE@users.noreply.github.com>
2023-07-17 15:54:33 +0530

    Time: 1 ms (66.69%), Space: 41 MB (94.82%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,43 +15,26 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<List<Integer>> temp = levelOrder(root);
         List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<temp.size();i++)
-        {
-            List<Integer> arr = temp.get(i);
-            ans.add(arr.get(arr.size()-1));
-        }
-        return ans;
-    }
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        if(root==null) return new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null) return ans;
         int level = height(root)+1;
-        for(int i=1;i<=level;i++)
+        for(int i=0;i<level;i++)
         {
-            List<Integer> temp = new ArrayList<>();
-            fun(root,i,temp);
-            ans.add(temp);
+            ans.add(100);
         }
+        preOrder(root,ans,1);
         return ans;
     }
-    
+    public static void preOrder(TreeNode root, List<Integer> ans,int level)
+    {
+        if(root==null) return;
+        ans.set(level-1,root.val);
+        preOrder(root.left,ans,level+1);
+        preOrder(root.right,ans,level+1);
+    }
     public static int height(TreeNode root)
     {
         if(root==null || (root.left==null && root.right==null)) return 0;
         return 1+Math.max(height(root.left),height(root.right));
-    }
-    
-    public static void fun(TreeNode node,int n,List<Integer> temp)
-    {
-        if(node==null) return;
-        if(n==1)
-        { 
-            temp.add(node.val); 
-            return;
-        }
-        fun(node.left,n-1,temp);
-        fun(node.right,n-1,temp);
     }
 }
