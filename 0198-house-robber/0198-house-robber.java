a2b310bc41a90a85a059f317f5e4153dfdb08e79
Rakshit <98008663+Rakshit-CSE@users.noreply.github.com>
2023-11-28 11:29:55 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (38.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,25 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return fun(nums,dp,0);
+        if(nums.length==1) return nums[0];
+        return fun(nums);
     }
     
-    public static int fun(int[] nums, int[] dp,int i)
+    public static int fun(int[] nums)
     {
-        if(i>=nums.length)
+        int dp[] = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++)
         {
-            return 0;
+            int rob = nums[i] + dp[i-2];
+            int do_ntrob = dp[i-1];
+            dp[i]=Math.max(rob,do_ntrob);
         }
+        return dp[nums.length-1];
         
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        
-        int rob = nums[i] + fun(nums,dp,i+2);
-        int do_ntrob = fun(nums,dp,i+1);
-        return dp[i]=Math.max(rob,do_ntrob);
     }
     
 }
