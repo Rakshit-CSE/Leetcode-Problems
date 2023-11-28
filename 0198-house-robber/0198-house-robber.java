fabe329938c5db19bbc03fefbe28d935a49579da
Rakshit <98008663+Rakshit-CSE@users.noreply.github.com>
2023-11-28 11:22:29 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (47.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,19 +5,21 @@ class Solution {
         return fun(nums,dp,0);
     }
     
-    public static int fun(int[] nums, int[] dp, int i)
+    public static int fun(int[] nums, int[] dp,int i)
     {
         if(i>=nums.length)
         {
             return 0;
         }
+        
         if(dp[i]!=-1)
         {
             return dp[i];
         }
-        int rob = nums[i]+fun(nums,dp,i+2);
+        
+        int rob = nums[i] + fun(nums,dp,i+2);
         int do_ntrob = fun(nums,dp,i+1);
-        dp[i] = Math.max(rob,do_ntrob);
-        return Math.max(rob,do_ntrob);
+        return dp[i]=Math.max(rob,do_ntrob);
     }
+    
 }
